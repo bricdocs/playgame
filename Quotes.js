7e34bacd5b0af2382384d9cd15a4d192cea0e991
bricdocs <bricdocs.web@gmail.com>
2025-03-09 19:41:51 +0300

    Create Quotes.js

---
+++ Quotes.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name" : "Chris Christie",
+        "quote": "I closed the bridge!",
+        "date" : "February 1, 2014",
+        "link" : "https://somelinktosomewhere.com"
+    },
+    {
+        "name" : "Hillary Clinton",
+        "quote": "Vote for me if I run next election!",
+        "date" : "March 31, 2014",
+        "link" : "https://somelinktosomewhere.com"
+    },    
+]
