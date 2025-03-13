791bf4aed5cd3725c623256b604757d6fee61e16
bricdocs <bricdocs.web@gmail.com>
2025-03-13 12:17:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "I have changed!";
- const myElement = document.getElementById("dene");
-  myElement.style.color = "blue";
-  myElement.innerHTML = "NONE"
-	
-}
-	function pullJSON() {  
-  var URL = "https://script.google.com/macros/s/AKfycbx-fw7DtK_Jto2qeLCKUqdZkzIfM4DYVRH0lsNdBHcqFxoL_XVWMKQ0_3mQVDBZDjLb/exec"
- var data = fetch(URL, {"contentType": "application/json"});
-  //console.log(data);
-document.getElementById("dene").innerHTML = data;
-
-}
-	//pullJSON();
-//document.getElementById("dene").innerHTML = "DENEME";
-
-
 //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 //https://github.com/facebook/react/issues/27219
-async function getQuotes() {
-const apiUrl = 'https://script.google.com/macros/s/AKfycbx-fw7DtK_Jto2qeLCKUqdZkzIfM4DYVRH0lsNdBHcqFxoL_XVWMKQ0_3mQVDBZDjLb/exec';
-try {
-const response = await fetch(apiUrl);
-const apiQuotes = await response.json();
-console.log(apiQuotes);
-	if(apiQuotes == "2"){
-		document.getElementById("btn0").disabled = false;
+async function getJson() {
+	const Url = 'https://script.google.com/macros/s/AKfycbx-fw7DtK_Jto2qeLCKUqdZkzIfM4DYVRH0lsNdBHcqFxoL_XVWMKQ0_3mQVDBZDjLb/exec';
+	try {
+		const response = await fetch(Url);
+		const data = await response.json();
+		console.log(data);
+		if(data[0][0] == 0){
+			document.getElementById("btn0").disabled = false;
+		}
+	} catch (error) {
+		console.log(error);
 	}
-} catch (error) {
-console.log(error);
-}
 }
 
-getQuotes();
+getJson();
