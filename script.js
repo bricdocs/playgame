dc2f52dd6985178f2a0b5100b71200ab8cf8225d
bricdocs <bricdocs.web@gmail.com>
2025-03-13 03:42:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 <script>
 function myFunction() {
   document.getElementById("demo").innerHTML = "I have changed!";
-  const myElement = document.getElementById("demo");
-  myElement.style.color = "blue";
-  myElement.innerHTML = "NONE"
+ const myElement = document.getElementById("dene");
+myElement.style.color = "blue";
+myElement.innerHTML = "NONE"
+
 }
   </script>
