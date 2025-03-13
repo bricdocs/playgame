fbc04de03657908af778db246b4f03009408624a
bricdocs <bricdocs.web@gmail.com>
2025-03-13 04:16:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ function myFunction() {
 document.getElementById("dene").innerHTML = data;
 
 }
-	pullJSON();
-	//document.getElementById("dene").innerHTML = "DENEME";
+	//pullJSON();
+//document.getElementById("dene").innerHTML = "DENEME";
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+//https://github.com/facebook/react/issues/27219
+async function getQuotes() {
+const apiUrl = 'https://script.google.com/macros/s/AKfycbx-fw7DtK_Jto2qeLCKUqdZkzIfM4DYVRH0lsNdBHcqFxoL_XVWMKQ0_3mQVDBZDjLb/exec';
+try {
+const response = await fetch(apiUrl);
+const apiQuotes = await response.json();
+console.log(apiQuotes);
+} catch (error) {
+console.log(error);
+}
+}
+
+getQuotes();
