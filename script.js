eb7e2caa444d0d1f93c67426d69aa7b785ddbce2
bricdocs <bricdocs.web@gmail.com>
2025-03-13 04:06:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function myFunction() {
   myElement.innerHTML = "NONE"
 
 }
+	function pullJSON() {  
+  var URL = "https://script.google.com/macros/s/AKfycbx-fw7DtK_Jto2qeLCKUqdZkzIfM4DYVRH0lsNdBHcqFxoL_XVWMKQ0_3mQVDBZDjLb/exec"
+ var data = fetch(URL, {"contentType": "application/json"});
+  //console.log(data);
+document.getElementById("dene").innerHTML = data;
+
+}
+	pullJSON();
+	//document.getElementById("dene").innerHTML = "DENEME";
